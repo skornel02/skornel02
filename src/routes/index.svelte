d675706beac5035fc5d14e0eb95140587e2a9db9
Kornél <stefankornel02@google.com>
2022-03-25 16:33:26 +0100

    Added school information

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -81,15 +81,30 @@
 		<div class="my-auto">
 			<h2 class="mb-5">Education</h2>
 
+			
 			<div class="resume-item d-flex flex-column flex-md-row mb-5">
 				<div class="resume-content mr-auto">
-					<h3 class="mb-0">School</h3>
-					<div class="subheading mb-3">Level</div>
-					<div>Specialization</div>
-					<p>Relevant info</p>
+					<h3 class="mb-0">BMSZC Neumann János Informatikai Technikum</h3>
+					<div class="subheading mb-3">Vocational secondary school</div>
+					<div>Programming and networking</div>
+					<p>
+						After an intense English year I have learned the fundemantals of the IT industry for 4
+						years. While studying i also took part in a variety of IT competitions. 
+					</p>
 				</div>
 				<div class="resume-date text-md-right">
-					<span class="text-primary">From - To</span>
+					<span class="text-primary">2017 - 2022</span>
+				</div>
+			</div>
+			<div class="resume-item d-flex flex-column flex-md-row mb-5">
+				<div class="resume-content mr-auto">
+					<h3 class="mb-0">Budaörsi Herman Ottó</h3>
+					<div class="subheading mb-3">Primary school</div>
+					<div>Media production</div>
+					<p>I have worked on numerous projects as a cameraman and also as a video editor.</p>
+				</div>
+				<div class="resume-date text-md-right">
+					<span class="text-primary">2009 - 2017</span>
 				</div>
 			</div>
 		</div>
