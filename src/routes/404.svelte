dd66bd00f2d36735f28a0a8904fc36fc05495c34
Kornél <stefankornel02@gmail.com>
2022-03-28 20:41:31 +0200

    Added most competition results

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -1,9 +1,27 @@
-<script lang="ts" context="module">
-	
+<script lang="ts">
+	const totalCounter = 5;
+	let counter = totalCounter;
+	setInterval(() => {
+		counter--;
+	}, 1000);
 </script>
 
-<script>
-	export let title = "Not found";
-</script>
+<div id="content">
+	<h2>Page not found!</h2>
+	<p>Redirecting to main page in {counter}...</p>
+</div>
 
-<h1>Page not found!</h1>
+<svelte:head>
+	<meta http-equiv="refresh" content="{totalCounter}; url=/">
+</svelte:head>
+
+<style>
+	#content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		text-align: center;
+		min-height: 75vh;
+		max-height: 100vh;
+	}
+</style>
