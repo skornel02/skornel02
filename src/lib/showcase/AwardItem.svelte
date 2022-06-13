6f0cf24dc6bd780d8ba9220deef20ba054a63080
Kornél <stefankornel02@gmail.com>
2022-06-13 14:45:30 +0200

    New layout for cards Windmill fix

--- src/lib/showcase/AwardItem.svelte
+++ src/lib/showcase/AwardItem.svelte
@@ -9,37 +9,54 @@
 </script>
 
 <ShowcaseFrame id="{award.id}">
-	<div class:highlight="{award.id !== undefined}" class="icon text-warning mx-1">
-		<svelte:component this="{typeToIcon(award.icon)}" />
-	</div>
-	<span class="badge bg-primary rounded-pill">
-		{award.placement}
-	</span>
-	<div class="w-100 d-flex justify-content-between align-items-start">
-		<div class="ps-2 w-100">
-			<div class="fw-bold">{award.title}</div>
-			<div class="d-flex justify-content-end">
-				{#each award.links as link}
-					<ShowcaseLink link="{link}" />
-				{/each}
+	<div class="row">
+		<div class="col-12 d-flex justify-content-center order-0 order-sm-1">
+			<div class="flex-grow-0 flex-sm-grow-1 mr-auto" style="flex-basis: 0;">
+
 			</div>
-			{#if award.team !== undefined}
-				<div class="d-flex teams">
-					<p class="fw-light">Team:</p>
-					{#each award.team as member}
-						<TeamMember member="{member}" />
-					{/each}
+			<div class="d-flex justify-content-center">
+				<div class:highlight="{award.id !== undefined}" class="icon text-warning mx-1">
+					<svelte:component this="{typeToIcon(award.icon)}" />
 				</div>
+				<div>
+					<span class="badge bg-primary rounded-pill">
+						{award.placement}
+					</span>
+				</div>
+			</div>
+			<div class="flex-grow-1 ml-auto" style="flex-basis: 0;">
+				<p class="text-end">
+					<small>
+						{award.date}
+					</small>
+				</p>
+			</div>
+		</div>
+		<div class="col-12 my-1 order-1 order-sm-0">
+			<div class="fw-bold text-center">{award.title}</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12 col-sm-6 d-flex justify-content-center justify-content-sm-start teams">
+			{#if Array.isArray(award.team)}
+				<p class="fw-light">Team:</p>
+				{#each award.team as member}
+					<TeamMember member="{member}" />
+				{/each}
 			{/if}
+		</div>
+		<div class="col-12 col-sm-6 d-flex justify-content-center justify-content-sm-end mt-1">
+			{#each award.links as link}
+				<ShowcaseLink link="{link}" />
+			{/each}
 		</div>
 	</div>
 </ShowcaseFrame>
 
 <style>
 	.icon {
-		display: inline-block;
-		width: 32px;
-		height: 32px;
+		min-width: 32px;
+		min-height: 32px;
 	}
 
 	.badge {
--- src/lib/showcase/CertificateItem.svelte
+++ src/lib/showcase/CertificateItem.svelte
@@ -8,12 +8,33 @@
 </script>
 
 <ShowcaseFrame id="{certificate.id}">
-	<div class:highlight="{certificate.id !== undefined}" class="icon text-warning mx-4">
-		<svelte:component this="{typeToIcon(certificate.icon)}" />
+	<div class="row">
+		<div class="col-12 d-flex justify-content-center order-0 order-sm-1">
+			<div class="flex-grow-0 flex-sm-grow-1 mr-auto" style="flex-basis: 0;">
+
+			</div>
+			<div class="d-flex justify-content-center">
+				<div class:highlight="{certificate.id !== undefined}" class="icon text-warning mx-4">
+					<svelte:component this="{typeToIcon(certificate.icon)}" />
+				</div>
+			</div>
+			<div class="flex-grow-1 ml-auto" style="flex-basis: 0;">
+				<p class="text-end">
+					<small>
+						{certificate.date}
+					</small>
+				</p>
+			</div>
+		</div>
+		<div class="col-12 my-1 order-1 order-sm-0">
+			<div class="fw-bold text-center">{certificate.title}</div>
+		</div>
 	</div>
-	<div class="w-100 d-flex justify-content-between ">
-		<div class="pe-2 me-auto w-100">
-			<div class="fw-bold text-start">{certificate.title}</div>
+	<div class="row">
+		<div
+			class="d-none d-sm-block col-sm-6 d-flex justify-content-center justify-content-sm-start teams"
+		></div>
+		<div class="col-12 col-sm-6 d-flex justify-content-center justify-content-sm-end mt-1">
 			{#each certificate.links as link}
 				<ShowcaseLink link="{link}" />
 			{/each}
