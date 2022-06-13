dd4ec33424285a8be46acca145d84834d3016137
Kornél <stefankornel02@gmail.com>
2022-06-13 14:45:30 +0200

    New layout for cards Windmill fix

--- src/lib/showcase/ShowcaseFrame.svelte
+++ src/lib/showcase/ShowcaseFrame.svelte
@@ -2,7 +2,7 @@
 	export let id: string | undefined = undefined;
 </script>
 
-<li id="{id}" class="list-group-item d-flex justify-content-between align-items-center">
+<li id="{id}" class="list-group-item column justify-content-between align-items-center">
 	<slot />
 </li>
 
