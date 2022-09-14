82bec4b908d04747ac8d75ec3d4bdbfb3f9a18d6
Kornél <stefankornel02@google.com>
2022-09-14 15:05:52 +0200

    New informations added

--- src/lib/experiences/Education.svelte
+++ src/lib/experiences/Education.svelte
@@ -1,6 +1,19 @@
 <div class="my-auto d-flex flex-column">
 	<h2 class="mb-5">Education</h2>
-
+	<div class="resume-item d-flex flex-column flex-md-row mb-5">
+		<div class="resume-content mr-auto">
+			<h3 class="mb-0">University of Szeged</h3>
+			<div class="subheading mb-3">Institute of informatics</div>
+			<div>Engineering Information Technology</div>
+			<p>
+				Currently studying at the University of Szeged, Faculty of Informatics. I am in my 1st year of my 
+				3.5 year long degree. 
+			</p>
+		</div>
+		<div class="resume-date text-md-right">
+			<span class="text-primary">2022 - 2025 (?)</span>
+		</div>
+	</div>
 	<div class="resume-item d-flex flex-column flex-md-row mb-5">
 		<div class="resume-content mr-auto">
 			<h3 class="mb-0">BMSZC Neumann János Informatikai Technikum</h3>
@@ -20,7 +33,9 @@
 			<h3 class="mb-0">Budaörsi Herman Ottó</h3>
 			<div class="subheading mb-3">Primary school</div>
 			<div>Media production</div>
-			<p>I have worked on numerous projects as a cameraman and also as a video editor.</p>
+			<p>I have worked on numerous multimedia projects as a cameraman and also as a video editor. 
+				Furthermore I also made radio shows for the school.
+			</p>
 		</div>
 		<div class="resume-date text-md-right">
 			<span class="text-primary">2009 - 2017</span>
