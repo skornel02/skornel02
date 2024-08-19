bebe4f61b416ac582d947a50318818669669af7c
Kornél <stefankornel02@gmail.com>
2024-08-19 22:38:54 +0200

    Added new competition results Readme shakeup Updated dependencies

--- website/src/env.d.ts
+++ website/src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-import {Engine} from 'tsparticles-engine';
+import {Engine} from '@tsparticles/engine';
 
 interface ImportMetaEnv {
 	readonly PROD: boolean;
