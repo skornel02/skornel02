f6683eeca7a746634d96ea1ba60aac90fa2516b5
skornel02 <stefankornel02@gmail.com>
2024-09-22 14:10:28 +0000

    Deploying to gh-pages from  @ d7f6e8e521afa932c83b4c632f6b264426eacdf1 🚀

--- _astro/PostsSearch.DkRc6Brs.js
+++
@@ -1,1 +0,0 @@
-import{P as i}from"./PostsSearch.D158un55.js";import"./index.ibZwBGDH.js";import"./astro/assets-service.BwuVOJvr.js";import"./hrefs.xlMFv88Q.js";export{i as default};
---
+++ _astro/java.BOts1L2k.js
@@ -0,0 +1,6 @@
+const e="java.md",a="skills",t="java",o=`
+Java was my first *real* programming language. 
+
+As most kids that want to spend their times nowadays, I too played games on the computer. My classmates and friends all played Minecraft and we always had a blast together. We always wanted to create servers together for everyone to enjoy but to that end we needed to learn how to host servers and configure them properly. This role mostly was mine and as time went on I realized that creating my own plugin would be a good thing to do. That rabbit hole went very deep and a few years later I already worked with basic Java UIs. 
+
+After settling in my vocational highschool I started the Hazizz project with my newly found friends. We had a strict breakdown of roles and I got the "Backend" developer role. This was the first time I created a web server and I was fortunate enough to choose Spring Boot and Java. `,n={name:"Java",icon:"logos:java",order:12,detailed:!0,achievements:[],experience:[],educations:[],posts:[]},s={type:"content",filePath:"/home/runner/work/skornel02/skornel02/website/src/content/skills/java.md",rawData:void 0};export{s as _internal,o as body,a as collection,n as data,e as id,t as slug};
